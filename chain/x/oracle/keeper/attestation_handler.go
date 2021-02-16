7e87b49af1f3abbf786f50f8b7daf5cf5d0fc73d
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2021-02-16 12:48:38 +0300

    Compute average in price oracle

--- chain/x/oracle/keeper/attestation_handler.go
+++ chain/x/oracle/keeper/attestation_handler.go
@@ -42,9 +42,16 @@ func (a AttestationHandler) Handle(ctx sdk.Context, att types.Attestation, claim
 				return price[i].LT(price[j])
 			})
 
+			var calculatedPrice sdk.Int
+			if len(price) % 2 == 0 {
+				calculatedPrice = price[len(price)/2].Add(price[len(price)/2-1]).QuoRaw(2) // compute average
+			} else {
+				calculatedPrice = price[len(price)/2]
+			}
+
 			prices.List = append(prices.List, &types.Price{
 				Name:  name,
-				Value: price[len(price)/2],
+				Value: calculatedPrice,
 			})
 		}
 
