b04934af063b0238f9610cae292ef3b1a78aa114
Daniil Lashin <3121312+danil-lashin@users.noreply.github.com>
2021-02-24 16:21:52 +0300

    Fix orchestrator

--- orchestrator/relayer/src/batch_relaying.rs
+++ orchestrator/relayer/src/batch_relaying.rs
@@ -34,7 +34,7 @@ pub async fn relay_batches(
     let mut latest_batches = latest_batches.unwrap();
     latest_batches.reverse();
 
-    let nonce = web3.eth_get_transaction_count(eth_address).await;
+    let nonce = web3.eth_get_transaction_count(our_ethereum_address).await;
     if nonce.is_err() {
         return;
     }
