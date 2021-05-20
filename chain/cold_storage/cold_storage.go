f3630ce31cc90656caa5482bf00f5da483211e2b
Daniil Lashin <admin@lcor.ru>
2021-05-20 12:12:08 +0300

    Update minter cold storage

--- chain/cold_storage/cold_storage.go
+++ chain/cold_storage/cold_storage.go
@@ -1,4 +1,4 @@
 package cold_storage
 
-const MinterStorage = "Mx68001420733be1f8ca639112108a0eba7c19a365"
+const MinterStorage = "Mx7072558b2b91e62dbed78e9a3453e5c9e01fec5e"
 const EthereumStorage = "0x58BD8047F441B9D511aEE9c581aEb1caB4FE0b6d"
